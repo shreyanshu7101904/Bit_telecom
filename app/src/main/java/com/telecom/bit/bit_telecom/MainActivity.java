c11d3ed53255dea503a52c81347b401b8a3ef50c
Shreyanshu shankar <shreyanshu7101904@gmail.com>
2018-03-15 21:47:53 +0530

    new icon

--- app/src/main/java/com/telecom/bit/bit_telecom/MainActivity.java
+++ app/src/main/java/com/telecom/bit/bit_telecom/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_faculty) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
